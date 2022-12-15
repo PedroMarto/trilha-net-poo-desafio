b1b8e82d4d888a050b786625889298d754d84d18
PedroMarto <116028577+PedroMarto@users.noreply.github.com>
2022-12-15 15:19:54 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "19996739228", modelo: "Tijolão 1995", imei: "123456789-0", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Jogo da Cobrinha (SNAKE)");
+
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "19996739226", modelo: "14 PRO MAX", imei: "098765432-1", memoria: 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Waze");
